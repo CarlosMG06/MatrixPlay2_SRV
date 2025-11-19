83eb282883d5609a1b491090e5319d521a3f95bb
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2025-11-19 01:40:30 +0100

    Squashed commit of the following:
    commit 56b4a7898a005100c91d0d9013c26450da65b9df
    Author: AdrianAmec <adriancmec@gmail.com>
    Date:   Wed Nov 19 00:17:30 2025 +0100
    
        logs 2.2
    
    commit d2f33870414f4b7af76bdf98cbb46aef560ed12f
    Author: AdrianAmec <adriancmec@gmail.com>
    Date:   Tue Nov 18 23:51:19 2025 +0100
    
        logs 2.1
    
        añadidos manera en laque el servidor comparte datos de clientes, agregados mas input logs
    
    commit e75cc637601bea802bcf6ae2d0321cd4b89f3077
    Merge: dfbc3b1 5ce4eeb
    Author: AdrianAmec <adriancmec@gmail.com>
    Date:   Tue Nov 18 23:39:11 2025 +0100
    
        Merge pull request #4 from CarlosMG06/pre
    
        Pre
    
    commit dfbc3b114f61be2504132335d1f9ff2cf37dfc7c
    Author: AdrianAmec <adriancmec@gmail.com>
    Date:   Tue Nov 18 23:37:06 2025 +0100
    
        Revert "logs 2.0"
    
        This reverts commit f75c7fb0cb7140f5b2d7c81f3151eadb9346262a.
    
    commit f75c7fb0cb7140f5b2d7c81f3151eadb9346262a
    Author: AdrianAmec <adriancmec@gmail.com>
    Date:   Tue Nov 18 22:37:27 2025 +0100
    
        logs 2.0
    
        cambiado forma en la que se comparte datos de jugadores
    
    commit 22b8d69696dda9a2b3ac5010073a323618e7c5b2
    Author: AdrianAmec <adriancmec@gmail.com>
    Date:   Tue Nov 18 14:01:29 2025 +0100
    
        logs
    
        Utils para crear y actualizar logs

--- PlantillaSVpong/src/main/java/com/server/Main.java
+++ PlantillaSVpong/src/main/java/com/server/Main.java
@@ -30,7 +30,7 @@ public class Main extends WebSocketServer {
     private final ClientRegistry clients;
 
     /** Mapa d’estat per client (source of truth del servidor). Clau = name/id. */
-    public final Map<String, ClientData> clientsData = new HashMap<>();
+    public static final Map<String, ClientData> clientsData = new HashMap<>();
 
     public final PlayPong gameData;
 
@@ -71,6 +71,7 @@ public class Main extends WebSocketServer {
         // broadcast(msg.toString());
 
         getName(conn);
+        UtilsLog.info("nueva coneccion.");
     }
 
     @Override
@@ -80,7 +81,8 @@ public class Main extends WebSocketServer {
         if (gameData != null && gameData.isPlayer(conn,clients.nameBySocket(conn))) {
             gameData.closeGame();
         }
-        System.out.println("Cliente desconectado: " + name);
+        UtilsLog.info(name + "se desconecto.");
+        //System.out.println("Cliente desconectado: " + name);
     }
 
     @Override
@@ -105,15 +107,15 @@ public class Main extends WebSocketServer {
                 String clientName = obj.getString(Missatges.K_VALUE);
                 JSONArray namesUsed = clients.currentNames();
 
-                System.out.println("Nombres actuales en el server: "+namesUsed.toString());
-                System.out.println("entro "+clientName);
+                //System.out.println("Nombres actuales en el server: "+namesUsed.toString());
+                //System.out.println("entro "+clientName);
 
                 //comprueba si el nombre esta usado = true
                 for (int i = 0; i<namesUsed.length();i++){
-                    System.out.println("nombre usado:  "+namesUsed.get(i));
+                    //System.out.println("nombre usado:  "+namesUsed.get(i));
                         if(clientName.equals(namesUsed.get(i))){
                             
-                            System.out.println(clientName+ " ya usado!!");
+                            //System.out.println(clientName+ " ya usado!!");
                             used=true;
                         }
                 }
@@ -132,13 +134,15 @@ public class Main extends WebSocketServer {
                     
                     // Si es la Raspberry, la marcamos como especial
                     if ("raspberryClient".equals(clientName)) {
-                        System.out.println("Raspberry identificada!");
+                        //System.out.println("Raspberry identificada!");
+                        UtilsLog.info(clientName+" conectado");
                         sendTextToRaspberry("prueba");
                     }else {
                         clientsData.put(clientName,new ClientData(clientName));
                     }
                     clients.add(conn,clientName);
-                    System.out.println("Cliente conectado: " + clientName);
+                    //System.out.println("Cliente conectado: " + clientName);
+                    UtilsLog.info(clientName+" añadido a clientData (jugadores)");
                 }
 
                 
@@ -161,6 +165,7 @@ public class Main extends WebSocketServer {
                     gameData.setPlayersReady(gameData.getPlayersReady()+1);
                     if(gameData.getPlayersReady()==2){
                         
+                        UtilsLog.info("Juego empezado");
                         gameData.startGame();
                     }
                     break;
@@ -168,7 +173,7 @@ public class Main extends WebSocketServer {
 
                 case Missatges.C_MOVE :
                     JSONObject json = obj.optJSONObject(Missatges.K_VALUE);
-                    System.out.println(json.toString());
+                    //System.out.println(json.toString());
                     gameData.addInputs(json);
 
 
@@ -206,6 +211,7 @@ public class Main extends WebSocketServer {
 
     @Override
     public void onStart() {
+        UtilsLog.info("Servidor WebSocket iniciado en puerto " + getPort());
         System.out.println("Servidor WebSocket iniciado en puerto " + getPort());
         setConnectionLostTimeout(100);
         startTicker();
@@ -226,6 +232,7 @@ public class Main extends WebSocketServer {
     /** Registra un shutdown hook per aturar netament el servidor en finalitzar el procés. */
     private static void registerShutdownHook(Main server) {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            UtilsLog.warning("Aturant servidor (shutdown hook)...");
             System.out.println("Aturant servidor (shutdown hook)...");
             try {
                 server.stopTicker();      // <- atura el bucle periòdic
@@ -234,6 +241,7 @@ public class Main extends WebSocketServer {
                 e.printStackTrace();
                 Thread.currentThread().interrupt();
             }
+            UtilsLog.info("Servidor aturat.");
             System.out.println("Servidor aturat.");
         }));
     }
@@ -283,6 +291,7 @@ public class Main extends WebSocketServer {
         } catch (WebsocketNotConnectedException e) {
             String name = clients.cleanupDisconnected(to);
             clientsData.remove(name);
+            UtilsLog.error("Client desconnectat durant send: " + name);
             System.out.println("Client desconnectat durant send: " + name);
         } catch (Exception e) {
             e.printStackTrace();
